ec9ed19a663b842401b9c7698714e866713dfbed
Edward Faulkner <ef@alum.mit.edu>
2015-08-01 12:59:13 -0400

    nicer things

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -23,25 +23,60 @@ export default Ember.Controller.extend({
   listItems,
   showList: false,
   timings: [],
+  manyTimes: 10,
+  emberVersion: Ember.VERSION,
+
+  isProdBuild: Ember.computed(function() {
+    try {
+      Ember.assert("are we running prod?", false);
+      return true;
+    } catch (err) {
+      return false;
+    }
+  }),
+
+  stats: Ember.computed('timings.[]', function() {
+    let timings = this.get('timings');
+    if (timings.length === 0) {
+      return null;
+    }
+    return {
+      mean: Math.floor(timings.reduce((a,b) => a + b) / timings.length)
+    };
+  }),
 
   resetTest() {
     this.set('showList', false);
     return waitForRender();
   },
 
+  runTest() {
+    return this.resetTest().then(() => {
+      let startTime = new Date();
+      this.set('showList', true);
+      return waitForRender().then(() => {
+        let ms = (new Date()) - startTime;
+        this.get('timings').pushObject(ms);
+      });
+    });
+  },
+
   actions: {
     clear() {
       this.resetTest();
+      this.set('timings', []);
     },
     runOneTest() {
-      this.resetTest().then(() => {
-        let startTime = new Date();
-        this.set('showList', true);
-        return waitForRender().then(() => {
-          let ms = (new Date()) - startTime;
-          this.get('timings').pushObject(ms);
-        });
+      this.runTest();
+    },
+    renderMany() {
+      let counter = this.get('manyTimes');
+      let step = () => this.runTest().then(() => {
+        if (--counter > 0) {
+          return step();
+        }
       });
+      return step();
     }
   }
 });
